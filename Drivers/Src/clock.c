fe787c1e8c0861a02372672cf1b634005cf96abf
bhagyashree-shet <bhagyashreeshet9927@gmail.com>
2026-01-15 16:10:24 +0530

    Implement clock_enable function for GPIO clocks

---
+++ Drivers/Src/clock.c
@@ -0,0 +1,34 @@
+
+#include "clock.h"
+#include <stdint.h>
+
+/* RCC base address (STM32F103) */
+#define RCC_BASE       0x40021000UL
+
+/* APB2 peripheral clock enable register */
+#define RCC_APB2ENR    (*(volatile uint32_t *)(RCC_BASE + 0x18))
+
+void clock_enable(clk_id_t clk)
+{
+    switch (clk)
+    {
+        case CLOCK_GPIOA:
+            RCC_APB2ENR |= (1U << 2);   // IOPAEN
+            break;
+
+        case CLOCK_GPIOB:
+            RCC_APB2ENR |= (1U << 3);   // IOPBEN
+            break;
+
+        case CLOCK_GPIOC:
+            RCC_APB2ENR |= (1U << 4); //IOPCEN
+            break;
+
+        case CLOCK_AFIO:
+            RCC_APB2ENR |= (1U << 0);   // AFIOEN
+            break;
+
+        default:
+            break;
+    }
+}
