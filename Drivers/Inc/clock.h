4384913789acd583164edbb81999677f419aef35
bhagyashree-shet <bhagyashreeshet9927@gmail.com>
2026-01-15 16:09:18 +0530

    Add clock.h header file for clock management

---
+++ Drivers/Inc/clock.h
@@ -0,0 +1,15 @@
+
+#ifndef CLOCK_H_
+#define CLOCK_H_
+
+
+typedef enum {
+    CLOCK_GPIOA,
+    CLOCK_GPIOB,
+	CLOCK_GPIOC,
+    CLOCK_AFIO
+} clk_id_t;
+
+void clock_enable(clk_id_t clk);
+
+#endif /* CLOCK_H_ */
