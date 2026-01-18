d9621afd19050f9610449c7fb52ed5a10895a69d
bhagyashree-shet <bhagyashreeshet9927@gmail.com>
2026-01-18 21:05:13 +0530

    Add CLOCK_TIM2 to clk_id_t

--- Drivers/Inc/clock.h
+++ Drivers/Inc/clock.h
@@ -7,9 +7,11 @@ typedef enum {
     CLOCK_GPIOA,
     CLOCK_GPIOB,
 	CLOCK_GPIOC,
-    CLOCK_AFIO
+    CLOCK_AFIO,
+	CLOCK_TIM2
 } clk_id_t;
 
 void clock_enable(clk_id_t clk);
 
 #endif /* CLOCK_H_ */
+
